dacd6dcdbde03d76bcc8fa0dea79fc1c6d2bdd99
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-06-16 21:21:46 -0400

    added folders and 1st draft components added an images folder that will house all of the images used throughout the whole site. added a components folder the currently has a projects component. the project has a rough draft of a carousel from reasctstrap.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Projects from './components/Projects';
+// import woods2waters from 'https://imgur.com/a/DcGPG03'
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <nav>
+        <Nav fill tabs>
+          <NavItem><NavLink active href="#">Home</NavLink></NavItem>
+          <NavItem><NavLink href="#">Projects</NavLink></NavItem>
+          <NavItem><NavLink href="#">Skills</NavLink></NavItem>
+          <NavItem><NavLink href="#">Refrences</NavLink></NavItem>
+          <NavItem><NavLink href="#">Contact me </NavLink></NavItem>
+        </Nav>
+      </nav>
+      <div>
+        <Projects/>
+      </div>
     </div>
   );
 }
