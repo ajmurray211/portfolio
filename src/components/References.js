d7fd694dd02ba0a4cb1c38e2515a512477fb7c82
AJ Murray <ajmurray@Jamies-MacBook-Pro.local>
2022-07-02 15:17:33 -0400

    deleted old commented out code

--- src/components/References.js
+++ src/components/References.js
@@ -3,32 +3,6 @@ import { UncontrolledAccordion,AccordionItem, AccordionBody, AccordionHeader} fr
 
 const References = () => {
     return (
-        // <div>
-        //     <h2>References</h2>
-        //     <section id="References">
-        //         <CardGroup>
-        //             <Card>
-        //                 <CardImg alt="Card image cap" src="https://picsum.photos/318/180" top width="100%" />
-        //                 <CardBody>
-        //                     <CardTitle tag="h5"> Reference #1</CardTitle>
-        //                     <CardText>
-        //                         Discription of referance
-        //                     </CardText>
-        //                 </CardBody>
-        //             </Card>
-        //             <Card>
-        //                 <CardImg alt="Card image cap" src="https://picsum.photos/318/180" />
-        //                 <CardBody>
-        //                     <CardTitle tag="h5">Reference #2</CardTitle>
-        //                     <CardText>
-        //                         Ref 2 discription
-        //                     </CardText>
-        //                 </CardBody>
-        //             </Card>
-        //         </CardGroup>
-        //     </section>
-        // </div>
-
         <div>
             <h2>References</h2>
             <UncontrolledAccordion
