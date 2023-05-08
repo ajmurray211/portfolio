e046a912a36df56d7411d79274e6e428cab4f6e6
AJ Murray <aj.murr4y@gmail.com>
2023-05-07 23:00:48 -0400

    changed navbar still having trouble with the auto scrolling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import './App.css';
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Nav, NavItem, NavLink, Navbar, NavbarBrand, NavbarToggler, Offcanvas, OffcanvasBody, OffcanvasHeader } from 'reactstrap';
 import Projects from './components/Projects';
 import About from './components/About';
 import References from './components/References';
 import ContactMe from './components/ContactMe';
 import Skills from './components/Skills';
+import { useState } from 'react';
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
   return (
     <div className="App">
       <nav id='Nav'>
-        <Nav fill tabs>
-          <NavItem><NavLink active href="#">Home</NavLink></NavItem>
-          <NavItem><NavLink href="#About">About</NavLink></NavItem>
-          <NavItem><NavLink href="#Skills">Skills</NavLink></NavItem>
-          <NavItem><NavLink href="#Projects">Projects</NavLink></NavItem>
-          <NavItem><NavLink href="#References">References</NavLink></NavItem>
-          <NavItem><NavLink href="#ContactMe">Contact me </NavLink></NavItem>
-        </Nav>
+        <Navbar light>
+          <NavbarBrand href="/" id='brandName' className="me-auto ">
+            Alexander Murray
+          </NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="me-2" />
+          <Offcanvas className='offcanvasNav' direction='start' isOpen={collapsed} toggle={toggleNavbar}>
+            <OffcanvasHeader toggle={toggleNavbar}>
+              Offcanvas
+            </OffcanvasHeader>
+            <OffcanvasBody>
+              <NavItem ><NavLink active href="#">Home</NavLink></NavItem>
+              <NavItem ><NavLink href="#Nav">About</NavLink></NavItem>
+              <NavItem ><NavLink href="#Projects">Projects</NavLink></NavItem>
+              <NavItem ><NavLink href="#References">References</NavLink></NavItem>
+              <NavItem ><NavLink href="#Skills">Skills</NavLink></NavItem>
+              <NavItem ><NavLink href="#ContactMe">Contact me </NavLink></NavItem>
+            </OffcanvasBody>
+          </Offcanvas>
+        </Navbar>
+        <About />
       </nav>
       <div>
-        <About />
-        <Skills />
         <Projects />
+        <Skills />
         <References />
         <ContactMe />
       </div>
